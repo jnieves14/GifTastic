ee145a46f624482fdd05f92dbcd0e90cf0511075
Jannah June Nieves <jannahjune@gmail.com>
2019-08-02 11:46:44 -0700

    fix animated/still

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,15 @@
 var shows =["Mr. D", "Kim's Convenience", "Fresh Off the Boat", "Stranger Things", "Community"];   
 
 function displayGif() {
-    var shows = $(this).attr("data-title");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + shows + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    var shows = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        shows + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     //AJAX REQUEST USING THE GET METHOD
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).done(function(response) {
     //EMPITES THE DIV HOLDING THE PREVIOUS GIF RESULTS
         $("#resultsDiv").empty();
         console.log(response);
@@ -20,13 +21,13 @@ function displayGif() {
             //STORING THE RESULT ITEM'S RATING
             var rating = response.data[i].rating;
             //CREATING A PARAGRPAH TAG WITH THE RESULT ITEM'S RATING
-            var ratingDiv = $("<p>").text("Rating: " + rating);
+            var ratingDiv = $("<p>").html("Rating: " + rating);
             //CREATING ANIMATED IMAGES VAR
             var animated = response.data[i].images.fixed_height.url;
             //CREATING A STILL IMAGE VAR
-            var still = response.data[i].images.fixed_height_stil.url;
+            var still = response.data[i].images.fixed_height_still.url;
             //CREATING AN IMAGE TAG FOR THE SHOWS GIFS
-            var showsImg = $("<img class='showsImage'>");
+            var showsImg = $("<img class='sImage'>");
 
             //SETTING THE GIFS' DEFAULT TO STILL
             showsImg.attr("src", still);
@@ -43,16 +44,16 @@ function displayGif() {
 };
 
 //ONCLICK FUNCTOINS TO ANIMATE AND PAUSE GIFS
-$("#resultsDiv").on("click", ".showsImage", function() {
+$("#resultsDiv").on("click", ".sImage", function() {
     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
     var state = $(this).attr("data-state");
     // If the clicked image's state is still, onclick will animate gif based on data-animate value
     if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("src", $(this).data("data-animate"));
       $(this).attr("data-state", "animate");
     //If state !== still, clicking will pause the gif
     } else {
-      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("src", $(this).data("data-still"));
       $(this).attr("data-state", "still");
     }
   });
@@ -60,11 +61,11 @@ $("#resultsDiv").on("click", ".showsImage", function() {
 //FUNCTION FOR NEW BUTTONS (BASED ON USERS' SEARCH)
 function renderButtons () {
     $("#buttonsMain").empty ();
-    for (var i=0; i < shows.length; i++) {
+    for (var i = 0; i < shows.length; i++) {
         var addButton = $("<button class='button'>");
         addButton.addClass("shows");
         addButton.attr("data-name", shows[i]);
-        addButton.text(shows[i]);
+        addButton.html(shows[i]);
 
         $("#buttonsMain").append(addButton);
     }
@@ -81,5 +82,5 @@ $(".add-gif").on("click", function(event) {
 
 //CALLS BUTTON TO BE CREATED
 $(document).on("click", ".shows", displayGif);
-renderButtons;
+renderButtons();
 
